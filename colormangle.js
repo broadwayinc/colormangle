29dea8f58f1b81fa55274e6f5f0c85f3ce3a19df
Baksa Gimm <baksa@broadwayinc.com>
2021-03-04 00:39:00 +0900

    adjusted faded/black opacity

--- colormangle.js
+++ colormangle.js
@@ -372,7 +372,7 @@ export class ColorMangle {
                     } else if (target.includes('faded')) {
                         let t_key = m.replace('_faded', '');
                         if (scheme[t_key])
-                            scheme[m] = this.rgba(opacity.faded, scheme[t_key]).string;
+                            scheme[m] = this.adjustBrightness(15, scheme[t_key]);
                         else
                             scheme[m] = `rgba(0, 0, 0, ${opacity.black})`;
                     }
