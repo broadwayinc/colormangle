971c738cf569802836cf58b2d583e1a5106ab5e0
Baksa Gimm <baksa@broadwayinc.com>
2021-05-04 19:28:57 +0900

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const path = require('path');
 
-module.exports = {
-    //mode: 'development',
-    mode: 'production',
-    entry: './src/colormangle.js',
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: 'colormangle.js',
-    },
-    jest: {
-        "transform": {
-            "^.+\\.jsx?$": "babel-jest"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (env) => {
+    return {
+        mode: env.mode || 'development',
+        entry: path.resolve(__dirname, './src/colormangle.js'),
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: './colormangle.js',
+            libraryTarget: 'window'
+        },
+        // jest: {
+        //     "transform": {
+        //         "^.+\\.jsx?$": "babel-jest"
+        //     }
+        // },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
                 }
-            }
-        ]
-    },
-    optimization: {
-        usedExports: true,
-    },
+            ]
+        },
+        optimization: {
+            usedExports: true,
+        }
+    };
 };
